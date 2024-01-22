886d2e0762f755a5c536c79e27a319fed4ce45ea
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2024-01-22 19:40:23 +0100

    Add Lab3 Entity Travel

---
+++ Labolatorium3_app/Controllers/TravelController.cs
@@ -0,0 +1,88 @@
+﻿using Labolatorium3_app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labolatorium3_app.Controllers;
+
+public class TravelController : Controller
+{
+    private static Dictionary<int, Travel> _travels = new Dictionary<int, Travel>();
+
+    public ActionResult Index()
+    {
+        return View(_travels);
+    }
+
+    public ActionResult Details(int id)
+    {
+        if (_travels.TryGetValue(id, out var travel))
+        {
+            return View(travel);
+        }
+        return NotFound();
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Travel travel)
+    {
+        if (ModelState.IsValid)
+        {
+            int newId = _travels.Any() ? _travels.Keys.Max() + 1 : 1;
+            travel.Id = newId;
+            _travels.Add(newId, travel);
+            return RedirectToAction("Index");
+        }
+        return View(travel);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        if (_travels.TryGetValue(id, out var travel))
+        {
+            return View(travel);
+        }
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(Travel travel)
+    {
+        if (ModelState.IsValid)
+        {
+            if (_travels.ContainsKey(travel.Id))
+            {
+                _travels[travel.Id] = travel;
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+        return View(travel);
+    }
+
+    public ActionResult Delete(int id)
+    {
+        if (_travels.TryGetValue(id, out var travel))
+        {
+            return View(travel);
+        }
+        return NotFound();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        if (_travels.ContainsKey(id))
+        {
+            _travels.Remove(id);
+            return RedirectToAction("Index");
+        }
+        return NotFound();
+    }
+}
---
+++ Labolatorium3_app/Models/Travel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labolatorium3_app.Models;
+
+public class Travel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nazwa podróży jest wymagana.")]
+    [StringLength(100, ErrorMessage = "Nazwa podróży nie może przekraczać 100 znaków.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Data rozpoczęcia jest wymagana.")]
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+
+    [Required(ErrorMessage = "Data zakończenia jest wymagana.")]
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+
+    [Required(ErrorMessage = "Miejsce początkowe jest wymagane.")]
+    public string StartPlace { get; set; }
+
+    [Required(ErrorMessage = "Miejsce końcowe jest wymagane.")]
+    public string EndPlace { get; set; }
+
+    [Required(ErrorMessage = "Uczestnicy są wymagani.")]
+    public string Participants { get; set; }
+
+    [Required(ErrorMessage = "Przewodnik jest wymagany.")]
+    public string Przewodnik { get; set; }
+}
+
